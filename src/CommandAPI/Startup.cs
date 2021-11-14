d64774d87e3579214247e9ca2ec20179862fb513
shanshan zhou <zhoussneu@gmail.com>
2021-11-15 11:55:36 +1300

    clean appsettings.json and move sensitive items

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Npgsql;
 
 namespace CommandAPI
 {
@@ -24,8 +25,14 @@ namespace CommandAPI
         }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
-            (Configuration.GetConnectionString("PostgreSqlConnection")));
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString =
+                Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+
+            services.AddDbContext<CommandContext>   
+                (opt => opt.UseNpgsql(builder.ConnectionString));
             
             //SECTION 1. Add code below
             services.AddControllers();
